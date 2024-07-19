575883d2e1d8deb78ef0e6ebd685895a862792ce
Zé Matheus <josemw86@gmail.com>
2024-07-18 23:40:44 -0300

    commit

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -2,10 +2,13 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
+import chess.ChessPiece;
 
 public class Program {
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
--- src/src/boardgame/Board.java
+++ src/src/boardgame/Board.java
@@ -12,6 +12,14 @@ public class Board {
         pieces = new Piece[rows][collumns];
     }
 
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
     public int getRows() {
         return rows;
     }
